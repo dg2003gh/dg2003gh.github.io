d7b62a93497a9d6fd5a5d729fda2c31b588c76e9
dg2003 <dg2003gh@gmail.com>
2023-09-19 14:43:24 -0300

    update

--- assets/js/scripts/githubLastRepos.js
+++ assets/js/scripts/githubLastRepos.js
@@ -1,32 +1,86 @@
 import { Octokit } from "https://esm.sh/octokit";
 
-const TOKEN =
-  "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";
+const TOKEN = "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";
 
 const octokit = new Octokit({
   auth: TOKEN,
 });
 
-async function getRepositoryInfo(index) {
+async function getRepositories() {
   const requestRepos = await octokit.request("GET /users/dg2003gh/repos");
-  const repos = requestRepos.data;
+
+  if (requestRepos.status != 0 || 404) {
+    if (requestRepos.status != 422) {
+      return requestRepos.data;
+    } else {
+      console.error("ERROR ! Validation failed", requestRepos);
+    }
+  } else {
+    console.error("ERROR ! Repositories request failed, error", requestRepos);
+  }
+}
+
+async function getREADMEFile(index) {
+  let repos = await getRepositories();
   const requestREADMEFile = await octokit.request(
     `GET /repos/dg2003gh/${repos[index].name}/readme`,
     {
       headers: { Accept: "application/vnd.github.html" },
     }
   );
-  const firstProject = document.querySelector("#js-first-project");
-  const description = document.querySelector("#js-first-project-description");
-  const image = document.querySelector("#js-first-project__img");
 
-  console.log(requestREADMEFile);
-  image.src = `https://github.com/dg2003gh/${repos[index].name}/raw/main/screenshots/portfolio_miniature.png`;
-  description.innerHTML = repos[index].description;
-  firstProject.innerHTML = `<h1>${repos[index].name}</h1>
-  <p>${requestREADMEFile.data.html}</p>
-  <i>Main Language: ${repos[index].language}</i>
-    <a href="${repos[index].html_url}">GitHub Link<a> `;
+  if (requestREADMEFile.status != 0 || 404) {
+    if (requestREADMEFile.status != 422) {
+      return requestREADMEFile.data;
+    } else {
+      console.error("ERROR ! Validation failed", requestREADMEFile);
+    }
+  } else {
+    console.error(
+      "ERROR ! README.md file request failed, error",
+      requestREADMEFile
+    );
+  }
 }
 
-getRepositoryInfo(1);
+//javascript:(function(){var s='https://raw.githubusercontent.com/skratchdot/github-enhancement-suite/master/build/github-enhancement-suite.user.js",
+//t='text/javascript',d=document,n=navigator,e;(e=d.createElement('script')).src=s;e.type=t;d.getElementsByTagName('head')[0].
+//appendChild(e)})();doIt('');void('')
+
+async function setProjectMiniature(index) {
+  const image = document.querySelector("#js-first-project__img");
+  const card = document.querySelector("#js-first-project__card");
+  let repos = await getRepositories();
+
+  let miniature = `https://github.com/dg2003gh/${repos[index].name}/raw/main/`,
+    t = "png";
+  if (miniature.status != 0 || 404) {
+    image.src = miniature;
+  } else {
+    card.innerHTML = '<i class="c-grid__icon  devicon-github-original"></i>';
+  }
+}
+
+async function setRepositoryInfo(projectID, projectDescriptionID, index) {
+  let project = document.querySelector(`#${projectID}`);
+  let description = document.querySelector(`#${projectDescriptionID}`);
+  let repos = await getRepositories();
+  let readmeFile = await getREADMEFile(index);
+
+  description.innerHTML = repos[index].description;
+  project.innerHTML = `
+
+  <main>
+        ${readmeFile} 
+        <i>Main Language: ${repos[index].language}</i>
+    </main>
+  <footer>
+  <a href="${repos[index].html_url}">GitHub Link<a>
+  </footer>
+  
+   `;
+
+  setProjectMiniature(index);
+}
+
+setRepositoryInfo("js-first-project", "js-first-project-description", 1);
