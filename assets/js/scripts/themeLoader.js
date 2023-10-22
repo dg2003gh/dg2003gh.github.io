86509c5c36f4b8245b7595822e1bc75f2432c027
Douglas Guimarães <dg2003gh@gmail.com>
2023-10-21 23:51:48 -0300

    add selected persistence

--- assets/js/scripts/themeLoader.js
+++ assets/js/scripts/themeLoader.js
@@ -1,27 +1,34 @@
-function switchTheme(theme_name) {
-  if (theme_name === "Cappuccino") {
-    document.body.classList.remove("cioccolato-theme");
-    document.body.classList.remove("high-contrast-theme");
-  }
-  if (theme_name === "Cioccolato") {
-    document.body.classList.add("cioccolato-theme");
-    document.body.classList.remove("high-contrast-theme");
-  }
-  if (theme_name === "High-contrast") {
-    document.body.classList.add("high-contrast-theme");
-    document.body.classList.remove("cioccolato-theme");
-  }
+const themeButton = document.querySelectorAll(
+  "#c-header__theme-dropDown option"
+);
 
-  localStorage.setItem("theme", JSON.stringify(theme_name));
-}
+themeButton.forEach((option) => {
+  option.onclick = () => {
+    switch (option.value) {
+      case "Cappuccino":
+        document.body.classList.remove("cioccolato-theme");
+        document.body.classList.remove("high-contrast-theme");
+        break;
+      case "Cioccolato":
+        document.body.classList.add("cioccolato-theme");
+        document.body.classList.remove("high-contrast-theme");
+        break;
+      case "High-contrast":
+        document.body.classList.add("high-contrast-theme");
+        document.body.classList.remove("cioccolato-theme");
+        break;
+    }
+
+    localStorage.setItem("theme", JSON.stringify(option.value));
+  };
+});
 
 function loadTheme() {
   let getTheme = JSON.parse(localStorage.getItem("theme"));
   if (getTheme === "Cioccolato") {
     document.body.classList.add("cioccolato-theme");
     document.body.classList.remove("high-contrast-theme");
-  }
-  if (getTheme === "High-contrast") {
+  } else if (getTheme === "High-contrast") {
     document.body.classList.add("high-contrast-theme");
     document.body.classList.remove("cioccolato-theme");
   }
