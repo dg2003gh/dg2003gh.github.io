6411608c4043c4cf2823dc24b8f92b8645f2f93e
Douglas Guimarães <dg2003gh@gmail.com>
2023-10-25 13:30:50 -0300

    update header (still in progress)

--- assets/js/scripts/header.js
+++ assets/js/scripts/header.js
@@ -1,22 +1,29 @@
 const expandButton = document.querySelector(".js-header__expand-mode");
+const header = document.querySelector(".js-header");
+const menu = document.getElementById("c-header__menu");
+const mobileButton = document.getElementById("c-header__mobileButton");
+
+function headerHide() {
+  header.style =
+    "top: -45px; padding-bottom: 21px; background: var(--clr-hide-header)";
+  expandButton.classList.add("ri-arrow-drop-down-line");
+  expandButton.classList.remove("ri-arrow-drop-up-line");
+  expandButton.style = "position: absolute;top: 40px;";
+  expandButton.title = "Show header";
+}
+
+function headerShow() {
+  header.style = "";
+  expandButton.classList.add("ri-arrow-drop-up-line");
+  expandButton.classList.remove("ri-arrow-drop-down-line");
+  expandButton.style = "";
+  expandButton.title = "hide header";
+}
+
 expandButton.onclick = () => {
-  const header = document.querySelector(".js-header");
   let getTopValue = window
     .getComputedStyle(header, null)
     .getPropertyValue("top");
 
-  if (getTopValue == "5px") {
-    header.style =
-      "top: -45px; padding-bottom: 21px; background: var(--clr-hide-header)";
-    expandButton.classList.add("ri-arrow-drop-down-line");
-    expandButton.classList.remove("ri-arrow-drop-up-line");
-    expandButton.style = "position: absolute;top: 40px;";
-    expandButton.title = "Show header";
-  } else {
-    header.style = "";
-    expandButton.classList.add("ri-arrow-drop-up-line");
-    expandButton.classList.remove("ri-arrow-drop-down-line");
-    expandButton.style = "";
-    expandButton.title = "hide header";
-  }
+  getTopValue == "5px" ? headerHide() : headerShow();
 };
