42f00f28f1c3a7f7aac0d94a843707b2d876d26c
Douglas Guimarães <dg2003gh@gmail.com>
2023-11-01 18:47:23 -0300

    add typed and html lang change

--- assets/js/classes/Translator.js
+++ assets/js/classes/Translator.js
@@ -1,7 +1,9 @@
 export default class Translator {
-  constructor(lng, tagAttr) {
+  constructor(lng, tagAttr, typedOn, typedContainer) {
     this.lng = lng;
     this.tagAttr = tagAttr;
+    this.typedContainer = typedContainer;
+    this.typedOn = typedOn;
   }
 
   translate() {
@@ -14,6 +16,7 @@ export default class Translator {
         } else throw new Error("File not found!");
       })
       .then((language) => {
+        document.documentElement.lang = this.lng;
         const allDom = document.getElementsByTagName("*");
         for (let index = 0; index < allDom.length; index++) {
           const elem = allDom[index];
@@ -21,6 +24,14 @@ export default class Translator {
           if (key != null) {
             elem.innerHTML = language[key];
           }
+        }
+        if (this.typedOn == true && language.typedStrings != null) {
+          new Typed(this.typedContainer, {
+            strings: language.typedStrings,
+            loop: true,
+            typeSpeed: 200,
+            backSpeed: 200,
+          });
         }
       })
       .catch((error) => console.error(error));
