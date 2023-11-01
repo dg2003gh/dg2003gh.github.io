f77db1a4150b08e5f847d81860f514f835c06575
Douglas Guimarães <dg2003gh@gmail.com>
2023-11-01 18:49:10 -0300

    change theme logic to work with select tag

--- assets/js/scripts/themeLoader.js
+++ assets/js/scripts/themeLoader.js
@@ -1,34 +1,41 @@
-const themeOptions = document.querySelectorAll("#c-theme-dropdown ul li");
+const themeDropdowns = document.querySelectorAll(".js-theme-dropdown");
 
-themeOptions.forEach((option) => {
-  option.onclick = () => {
-    switch (option.dataset.theme) {
-      case "Cappuccino":
-        document.body.classList.remove("cioccolato-theme");
-        document.body.classList.remove("high-contrast-theme");
-        break;
-      case "Cioccolato":
-        document.body.classList.add("cioccolato-theme");
-        document.body.classList.remove("high-contrast-theme");
-        break;
-      case "High-contrast":
-        document.body.classList.add("high-contrast-theme");
-        document.body.classList.remove("cioccolato-theme");
-        break;
-    }
-    localStorage.setItem("theme", JSON.stringify(option.dataset.theme));
-  };
-});
-
-function loadTheme() {
-  let getTheme = JSON.parse(localStorage.getItem("theme"));
-  if (getTheme === "Cioccolato") {
-    document.body.classList.add("cioccolato-theme");
-    document.body.classList.remove("high-contrast-theme");
-  } else if (getTheme === "High-contrast") {
-    document.body.classList.add("high-contrast-theme");
-    document.body.classList.remove("cioccolato-theme");
+function chooseTheme(theme) {
+  switch (theme) {
+    case "Cappuccino":
+      document.body.classList.remove("cioccolato-theme", "high-contrast-theme");
+      break;
+    case "Cioccolato":
+      document.body.classList.add("cioccolato-theme");
+      document.body.classList.remove("high-contrast-theme");
+      break;
+    case "High-contrast":
+      document.body.classList.add("high-contrast-theme");
+      document.body.classList.remove("cioccolato-theme");
+      break;
   }
 }
+
+function loadTheme() {
+  const getTheme = JSON.parse(localStorage.getItem("theme"));
+  const options = document.querySelectorAll(".js-theme-dropdown option");
+
+  if (getTheme == "" || getTheme == null) return;
+
+  options.forEach((option) => {
+    option.value == getTheme
+      ? (option.selected = true)
+      : (option.selected = false);
+  });
+
+  chooseTheme(getTheme);
+}
+
+themeDropdowns.forEach((themeDropdown) => {
+  themeDropdown.addEventListener("click", () => {
+    chooseTheme(themeDropdown.value);
+    localStorage.setItem("theme", JSON.stringify(themeDropdown.value));
+  });
+});
 
 loadTheme();
