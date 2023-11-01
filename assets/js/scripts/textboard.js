aa59738deea358e41055c01c158d9f610d46a820
Douglas Guimarães <dg2003gh@gmail.com>
2023-11-01 18:49:54 -0300

    update switches logic

--- assets/js/scripts/textboard.js
+++ assets/js/scripts/textboard.js
@@ -1,28 +1,25 @@
 const boardSwitches = document.querySelectorAll(".c-textBoard__switch");
 
-boardSwitches.forEach((boardSwitch, switchIndex) => {
+boardSwitches.forEach((boardSwitch) => {
   boardSwitch.onclick = () => {
     const boardTextContainer = document.querySelectorAll(
       ".c-textBoard__text-container .c-textBoard__text"
     );
     const switchContainer = document.getElementById("c-textBoard__switches");
-    if (switchContainer.style.getPropertyValue("--js-right") == "0") {
-      switchContainer.style.setProperty("--js-right", "auto");
-      switchContainer.style.setProperty("--js-left", "0");
+    if (
+      switchContainer.style.getPropertyValue("--js-translateX") == "0px" ||
+      switchContainer.style.getPropertyValue("--js-translateX") == ""
+    ) {
+      switchContainer.style.setProperty("--js-translateX", "100% 0px");
     } else {
-      switchContainer.style.setProperty("--js-right", "0");
-      switchContainer.style.setProperty("--js-left", "auto");
+      switchContainer.style.setProperty("--js-translateX", "0px");
     }
 
-    boardTextContainer.forEach((text, textIndex) => {
-      if (text.style.transform.includes("100")) {
-        text.style = `
-        transform: translateX(-100%);
-        `;
+    boardTextContainer.forEach((text) => {
+      if (text.style.translate == "0px" || text.style.translate == "") {
+        text.style.translate = "-100% 0px";
       } else {
-        text.style = `
-        transform: translateX(100%);
-        `;
+        text.style.translate = "0px";
       }
     });
   };
