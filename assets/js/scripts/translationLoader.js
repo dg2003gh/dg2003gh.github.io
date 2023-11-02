0274e8e6d52545a6fc039c8568d0adacbd5f9f45
Douglas Guimarães <dg2003gh@gmail.com>
2023-11-02 11:10:04 -0300

    add get navigator lang if getLng is empty

--- assets/js/scripts/translationLoader.js
+++ assets/js/scripts/translationLoader.js
@@ -3,22 +3,26 @@ import Translator from "../classes/Translator.js";
 const languageDropdowns = document.querySelectorAll(".js-header__languages");
 
 function setTranslation(lng) {
+  const options = document.querySelectorAll(".js-header__languages option");
   const translator = new Translator(lng, "translate-key", true, "#c-typedWord");
   translator.translate();
   localStorage.setItem("Language", JSON.stringify(lng));
+
+  options.forEach((option) => {
+    option.value == lng ? (option.selected = true) : (option.selected = false);
+  });
 }
 
 function loadTranslation() {
   const getLng = JSON.parse(localStorage.getItem("Language"));
-  const options = document.querySelectorAll(".js-header__languages option");
 
-  options.forEach((option) => {
-    option.value == getLng
-      ? (option.selected = true)
-      : (option.selected = false);
-  });
-
-  setTranslation(getLng);
+  if (getLng == null || getLng == "") {
+    const userLang = navigator.language || navigator.userLanguage;
+    console.log(userLang);
+    setTranslation(userLang);
+  } else {
+    setTranslation(getLng);
+  }
 }
 
 languageDropdowns.forEach((languageDropdown) => {
