1d56dcf92115238e7b0325d4577ebb6fcbf3d88c
Douglas Guimarães <dg2003gh@gmail.com>
2023-11-02 11:08:34 -0300

    add fallback en lang

--- assets/js/classes/Translator.js
+++ assets/js/classes/Translator.js
@@ -1,22 +1,41 @@
 export default class Translator {
-  constructor(lng, tagAttr, typedOn, typedContainer) {
+  constructor(
+    lng,
+    tagAttr,
+    typedOn,
+    typedContainer = null,
+    typedOptions = {
+      strings: [],
+      loop: true,
+      typeSpeed: 200,
+      backSpeed: 200,
+    }
+  ) {
     this.lng = lng;
     this.tagAttr = tagAttr;
     this.typedContainer = typedContainer;
     this.typedOn = typedOn;
+    this.typedOptions = typedOptions;
+    this.typed;
   }
 
   translate() {
     if (this.lng == null || this.lng == "") return;
 
-    fetch("./assets/translations/" + this.lng + ".json")
+    const options = {
+      headers: {
+        "Accept-Encoding": "br",
+      },
+    };
+
+    fetch("./assets/translations/" + this.lng + ".json", options)
       .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else throw new Error("File not found!");
+        if (response.ok) return response.json();
+        else throw new Error("File not found!");
       })
       .then((language) => {
         document.documentElement.lang = this.lng;
+
         const allDom = document.getElementsByTagName("*");
         for (let index = 0; index < allDom.length; index++) {
           const elem = allDom[index];
@@ -25,15 +44,20 @@ export default class Translator {
             elem.innerHTML = language[key];
           }
         }
-        if (this.typedOn == true && language.typedStrings != null) {
-          new Typed(this.typedContainer, {
-            strings: language.typedStrings,
-            loop: true,
-            typeSpeed: 200,
-            backSpeed: 200,
-          });
+
+        if (
+          this.typedOn == true &&
+          language.typedStrings != null &&
+          this.typedContainer != null
+        ) {
+          this.typedOptions.strings = language.typedStrings;
+          this.typed = new Typed(this.typedContainer, this.typedOptions);
         }
       })
-      .catch((error) => console.error(error));
+      .catch((error) => {
+        this.lng = "en";
+        this.translate();
+        console.error(error + " get english translation...");
+      });
   }
 }
