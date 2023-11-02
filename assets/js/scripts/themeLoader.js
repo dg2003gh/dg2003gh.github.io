f99efaa31f2db4f82aca555ec26f9d0255add26e
Douglas Guimarães <dg2003gh@gmail.com>
2023-11-02 12:12:23 -0300

    fixed - not working on mobile

--- assets/js/scripts/themeLoader.js
+++ assets/js/scripts/themeLoader.js
@@ -1,8 +1,6 @@
 const themeDropdowns = document.querySelectorAll(".js-theme-dropdown");
 
 function chooseTheme(theme) {
-  const options = document.querySelectorAll(".js-theme-dropdown option");
-
   switch (theme) {
     case "Cappuccino":
       document.body.classList.remove("cioccolato-theme", "high-contrast-theme");
@@ -16,27 +14,21 @@ function chooseTheme(theme) {
       document.body.classList.remove("cioccolato-theme");
       break;
   }
-
-  options.forEach((option) => {
-    option.value == theme
-      ? (option.selected = true)
-      : (option.selected = false);
-  });
 }
 
 function loadTheme() {
   const getTheme = JSON.parse(localStorage.getItem("theme"));
 
-  if (getTheme == "" || getTheme == null) return;
-
   chooseTheme(getTheme);
-}
 
-themeDropdowns.forEach((themeDropdown) => {
-  themeDropdown.addEventListener("click", () => {
-    chooseTheme(themeDropdown.value);
-    localStorage.setItem("theme", JSON.stringify(themeDropdown.value));
+  themeDropdowns.forEach((themeDropdown) => {
+    themeDropdown.addEventListener("change", () => {
+      chooseTheme(themeDropdown.value);
+      localStorage.setItem("theme", JSON.stringify(themeDropdown.value));
+    });
+
+    themeDropdown.value = getTheme;
   });
-});
+}
 
 loadTheme();
