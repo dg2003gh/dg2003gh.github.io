d276c4c4099ccec1a02837a5c299bc9bfbc7258f
Douglas Guimarães <dg2003gh@gmail.com>
2023-11-07 15:55:43 -0300

    add destruct on mobile condition

--- assets/js/scripts/modelConfig.js
+++ assets/js/scripts/modelConfig.js
@@ -1,4 +1,14 @@
 import ModelsRender from "../classes/ModelsRender.js";
 
 const container = document.getElementById("c-threejs__container");
-new ModelsRender(container, 75, 0.01, 1000, 12, "/assets/models/scene.gltf");
+
+const roomModel = new ModelsRender(
+  container,
+  75,
+  0.01,
+  1000,
+  12,
+  "/assets/models/scene.gltf"
+);
+
+innerWidth <= 700 ? roomModel.destructor() : roomModel.init();
